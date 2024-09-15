14f20e26f2addbfc86b0fed4f5f90f1d61a37fff
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-09-16 04:20:47 +0900

    New: GitCommit :: Pick(string $commit_id) : bool

--- GitCommit.class.php
+++ GitCommit.class.php
@@ -39,4 +39,30 @@ class GitCommit implements IF_UNIT
 	 *
 	 */
 	use OP_CORE, OP_CI;
+
+	/** Pick
+	 *
+	 * @created    2024-09-16
+	 * @param      string     $commit_id
+	 * @return     bool
+	 */
+	static function Pick(string $commit_id) : bool
+	{
+		/* @var $output array */
+		/* @var $status int   */
+		$result = exec("git cherry-pick {$commit_id} 2>&1", $output, $status);
+
+		//	...
+		if( $result === 'no changes added to commit (use "git add" and/or "git commit -a")' ){
+			$status = 0;
+		}
+
+		//	...
+		if( $status ){
+			var_dump($result, $status, $output);
+		}
+
+		//	...
+		return ($status === 0) ? true: false;
+	}
 }
