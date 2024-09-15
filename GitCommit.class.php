7890a36b419d2d43656417153b1c1a28bb76fef4
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-09-16 04:19:28 +0900

    New: GitCommit.class.php

---
+++ GitCommit.class.php
@@ -0,0 +1,42 @@
+<?php
+/** op-unit-git:/GitCommit.class.php
+ *
+ * @created     2024-09-16
+ * @version     1.0
+ * @package     op-unit-git
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\GIT;
+
+/** use
+ *
+ */
+use OP\OP_CORE;
+use OP\OP_CI;
+use OP\IF_UNIT;
+
+/** GitCommit
+ *
+ * @created     2024-09-16
+ * @version     1.0
+ * @package     op-unit-git
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+class GitCommit implements IF_UNIT
+{
+	/** use
+	 *
+	 */
+	use OP_CORE, OP_CI;
+}
