83f6ff6985c993efb94468d931c87ec3d5221090
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-09-06 22:35:51 +0900

    Fix: GitBranch :: Current() | If git branch was failed, throw exception.

--- GitBranch.class.php
+++ GitBranch.class.php
@@ -58,10 +58,15 @@ class GitBranch implements IF_UNIT
 	static function Current() : string
 	{
 		/*
-		return trim(`git rev-parse --abbrev-ref HEAD 2>&1`);
-		return trim(`git symbolic-ref --short HEAD 2>&1`);
+		$branch = `git rev-parse --abbrev-ref HEAD 2>&1`;
+		$branch = `git symbolic-ref --short HEAD 2>&1`;
 		*/
-		return trim(`git branch --show-current 2>&1`);
+		if(!$branch = `git branch --show-current 2>&1` ){
+			throw new \Exception("git branch was failed: " . getcwd());
+		}
+
+		//	...
+		return trim($branch);
 	}
 
 	/** Add branch repository.
