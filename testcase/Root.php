96b9b7aafd8f69ad3ec576faba7df469412aa05d
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-30 18:59:04 +0900

    Add: testcase > Root.php | PHPDoc

--- testcase/Root.php
+++ testcase/Root.php
@@ -1,4 +1,22 @@
 <?php
+/**	op-unit-git:/testcase/Root.class.php
+ *
+ * @created    2025-10-29
+ * @version    1.0
+ * @package    op-unit-git
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
 
 /* @var $git \OP\UNIT\Git */
 $git  = OP()->Unit('Git');
